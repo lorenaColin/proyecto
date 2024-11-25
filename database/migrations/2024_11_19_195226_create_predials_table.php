598e761639e9fd8d7fed7525110b5a7bfd463247
Jesus Cap <capa.2901@gmail.com>
2024-11-25 10:56:34 -0600

    names y corrections db

--- database/migrations/2024_11_19_195226_create_predials_table.php
+++ database/migrations/2024_11_19_195226_create_predials_table.php
@@ -14,6 +14,19 @@ return new class extends Migration
         Schema::create('predials', function (Blueprint $table) {
             $table->id();
             $table->string('account',50);
+
+            $table->foreignId('invoice_detail_id')
+                ->nullable()
+                ->constrained('invoice_details')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->foreignId('quotation_detail_id')
+                ->nullable()
+                ->constrained('quotation_details')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
             $table->timestamps();
         });
     }
